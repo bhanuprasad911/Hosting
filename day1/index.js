b89046b424a755d8c0cc7b34ae972d488976bff4
bhanuprasad911 <107980074+bhanuprasad911@users.noreply.github.com>
2024-11-16 21:07:04 +0530

    Add files via upload

---
+++ day1/index.js
@@ -0,0 +1,10 @@
+console.log("Hello world..!")
+let i = 10
+let j = 12
+let k = "10"
+console.log(i+j)
+console.log(j+k)
+console.log(i==j)
+console.log(i==k)
+console.log(i===k)
+console.log("Bhanu")
